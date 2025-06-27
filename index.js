e59eeec3d2db51954374cb340c54ddb6d2b4b237
ansifmk <mohammedansifmka@gmail.com>
2025-06-27 11:03:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+let car={
+    Brand:"bmw",
+    model:911,
+    year:2022,
+    color:"blue",
+}
+function ans(obj){
+let arr=[]
+for(key in car){
+    arr.push(key,car[key])
+}
+}
+
+console.log(arr);
+let print=""
+for(Element of print){
+
+
+}console.log(print);
+
+
+
+
