cec0c7039d9b864c75d266c432fde5c9ad4e56f4
ansifmk <mohammedansifmka@gmail.com>
2025-07-04 14:48:07 +0530

    Add files via upload

--- index2.js
+++ index2.js
@@ -1,34 +1,62 @@
-const student=[
-    {name:"ansif",score:90},
-    {name:"rashi",score:55},
-    {name:"fasil",score:80},
-    {name:"aflu",score:85},
-    {name:"arjun",score:20},
-]
-console.log(student);
+//daily task =1
 
-const names=student.map((students)=>students.name);
-console.log(names);
+// const student=[
+//     {name:"ansif",score:90},
+//     {name:"rashi",score:55},
+//     {name:"fasil",score:80},
+//     {name:"aflu",score:85},
+//     {name:"arjun",score:20},
+// ]
+// console.log(student);
 
-const nam=student.filter((student)=>student.score>75);
-console.log(nam);
+// const names=student.map((students)=>students.name);
+// console.log(names);
 
-const totalScore = student.reduce((sum, student)=>sum + student.score, 0);
-const averageScore = totalScore / student.length;
-console.log(averageScore)
+// const nam=student.filter((student)=>student.score>75);
+// console.log(nam);
 
-function grade(score){
-    if(score>=90){
-        return 'A'
-    }else if(score>=70){
-        return 'B'
-    }else{
-        return 'c'
-    }
+// const totalScore = student.reduce((sum, student)=>sum + student.score, 0);
+// const averageScore = totalScore / student.length;
+// console.log(averageScore)
+
+// function grade(score){
+//     if(score>=90){
+//         return 'A'
+//     }else if(score>=70){
+//         return 'B'
+//     }else{
+//         return 'c'
+//     }
+// }
+// console.log("name:ansif,garde:", grade(90));
+// console.log("name:rashi,garde:", grade(55));
+// console.log("name:fasil,garde:", grade(80));
+// console.log("name:aflu,garde:", grade(85));
+// console.log("name:arjun,garde:", grade(20));
+
+
+
+//dailytask 2
+
+function factorial(n){
+if(n===0 || n===1){
+    return 1;
 }
-console.log("name:ansif,garde:", grade(90));
-console.log("name:rashi,garde:", grade(55));
-console.log("name:fasil,garde:", grade(80));
-console.log("name:aflu,garde:", grade(85));
-console.log("name:arjun,garde:", grade(20));
+return n*factorial(n-1)
+}
+console.log(factorial(5));
 
+function sum(n){
+    if(n===0){
+        return 0;
+    }
+    return n+sum(n-1);
+}
+console.log(sum(5));
+
+
+function operate(num,callback){
+    return callback(num);
+
+}console.log(operate(5,factorial));
+console.log(operate(5,sum));
